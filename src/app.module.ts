73b9da94f4bb9c8c6b1f764e4887ca6f7d23ac88
peter-mwaura <peterkaranjagitz@gmail.com>
2025-06-11 02:43:21 +0300

    feat(statement): initialize statement module, controller, and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { StatementModule } from './statement/statement.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, StatementModule],
   controllers: [AppController],
   providers: [AppService],
 })
